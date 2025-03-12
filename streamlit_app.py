100c12dba8c80df41ab8816da47344441e652268
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-03-12 23:44:15 +0000

    h

--- streamlit_app.py
+++ streamlit_app.py
@@ -201,8 +201,8 @@ def home_page():
         forecast_df = forecast_df.tz_localize('CET').tz_convert('UTC')
         forecast_df = forecast_df.resample('1h').mean()
         if len(forecast_df) > 36:
-            forecast_df.loc[forecast_df.index[60:], 'meteofrance_0.5'] = np.nan
-            forecast_df.loc[forecast_df.index[60:], 'icon_0.5'] = np.nan
+            forecast_df.loc[forecast_df.index[44:], 'meteofrance_0.5'] = np.nan
+            forecast_df.loc[forecast_df.index[44:], 'icon_0.5'] = np.nan
 
        
         # Extract the date range from the forecast data
