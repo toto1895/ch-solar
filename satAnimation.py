7144c871b78f1910a9b06e6321b35dcadf88c2ab
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-05-04 10:30:31 +0000

    ooo

--- satAnimation.py
+++ satAnimation.py
@@ -483,7 +483,7 @@ def generate_sat_rad_anim():
     # Download and open the files
     datasets = download_and_open_nc_files(conn, files)
     
-    print(datasets)
+
     # Concatenate the datasets
     combined_dataset = concat_datasets(datasets)
     
