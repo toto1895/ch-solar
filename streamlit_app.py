5565a4d5dce5c06826d58ba2a2731a5774259b87
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-05-04 22:14:13 +0000

    fff

--- streamlit_app.py
+++ streamlit_app.py
@@ -697,7 +697,7 @@ def main():
         b_code="""
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="wamine" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
         """
-        components.html(b_code, height=600)
+        components.html(b_code, height=500)
 
     if page_choice == "Home":
         home_page()
