5d6c710705d165de651ca399cc096e325eae05ae
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-05-16 22:45:59 +0000

    fgggg

--- streamlit_app.py
+++ streamlit_app.py
@@ -641,13 +641,10 @@ def about_page():
 from satAnimation import generate_sat_rad_anim
 def sat_anim():
     fig_anim = generate_sat_rad_anim()
-
     st.plotly_chart(fig_anim, use_container_width=True, theme=None)
 
 from satAnimation_icon import  display_png
 def sat_anim_ch1():
-    #fig_anim = generate_solar_radiation_plots(num_plots=32)
-    #st.pyplot(fig_anim)
     display_png()
 
 from satAnimation_icon_ch2 import display_png
