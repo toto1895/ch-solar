19ee36861b546c0d50fc440605ad0b776898b917
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-05-20 20:25:12 +0000

    jjr

--- streamlit_app.py
+++ streamlit_app.py
@@ -563,7 +563,6 @@ def home_page():
                 var_name='Canton',         # Name for the variable column
                 value_name='Pronovo'       # Name for the value column
             )
-
             # If you want to sort by datetime
             pronovo_long = pronovo_long.sort_values('datetime').reset_index(drop=True)
             filtered_df = pd.merge(filtered_df, pronovo_long, on=["datetime","Canton"], how="left")
