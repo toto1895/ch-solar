afb5a97dbec8a7232c1478bd6fc630d809264f7c
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-05-20 20:19:16 +0000

    rrr

--- streamlit_app.py
+++ streamlit_app.py
@@ -558,7 +558,7 @@ def home_page():
             capa_installed =filtered_df.loc[filtered_df.datetime == filtered_df.datetime.max()
                                                    ].groupby('datetime')['cum_operator'].sum().values[0]
             st.success(f"Declared installed capacity: {round(capa_installed/1000):,.0f} MW  ( Today ~{1.1*round(capa_installed/1000):,.0f} MW) ")
-            
+            st.dataframe(filtered_df)
             # Calculate power metrics
             filtered_df['p0.5_canton'] = 1.1*filtered_df['p0.5'] * filtered_df['cum_canton'] / 1000
             filtered_df['p0.1_canton'] = 1.1*filtered_df['p0.1'] * filtered_df['cum_canton'] / 1000
