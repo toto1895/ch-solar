ddeee9a48dca7d443110833d33b8973bac38cd4c
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-06-05 06:10:30 +0000

    jj

--- streamlit_app.py
+++ streamlit_app.py
@@ -568,9 +568,6 @@ def home_page():
             st.dataframe(pronovo_f)
             pronovo_long = pronovo_long.sort_values('datetime').reset_index(drop=True)
             
-            #filtered_df = pd.merge(filtered_df, pronovo_long, on=["datetime","Canton"], how="left")
-            #st.dataframe(filtered_df)
-            
             # Calculate power metrics
             filtered_df['p0.5_canton'] = 1.1*filtered_df['p0.5'] * filtered_df['cum_canton'] / 1000
             filtered_df['p0.1_canton'] = 1.1*filtered_df['p0.1'] * filtered_df['cum_canton'] / 1000
