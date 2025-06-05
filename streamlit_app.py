e653a98493d0179f66d8c36938284837a91c026e
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-06-05 06:14:21 +0000

    k

--- streamlit_app.py
+++ streamlit_app.py
@@ -565,7 +565,7 @@ def home_page():
                 value_name='Pronovo'       # Name for the value column
             )
             pronovo_f = pd.merge(pronovo_long,filtered_df, on=["datetime","Canton"], how="left")
-            st.dataframe(pronovo_f)
+            st.dataframe(filtered_df)
             pronovo_long = pronovo_long.sort_values('datetime').reset_index(drop=True)
             
             filtered_df['p0.5_canton'] = 1.1*filtered_df['p0.5'] * filtered_df['cum_canton'] / 1000
