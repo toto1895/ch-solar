6daba764fc92218fd66b41f3b3275a2a9b7869d3
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-06-08 09:20:37 +0000

    hhh

--- streamlit_app.py
+++ streamlit_app.py
@@ -586,6 +586,7 @@ def home_page():
             filtered_df['datetime'] = pd.to_datetime(filtered_df['datetime'])
             pronovo_long['datetime'] = pd.to_datetime(pronovo_long['datetime'])
 
+
             pronovo_f = pd.merge(pronovo_long,capa_df, on=["datetime","Canton"], how="left")
             
             st.dataframe(pronovo_f)
