fb3706e119b904293a52a470ff4253bf105d49b6
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-06-11 06:14:46 +0000

    gfdg

--- streamlit_app.py
+++ streamlit_app.py
@@ -509,8 +509,8 @@ def home_page():
                 merged_df = pd.merge(forecast_df.reset_index(), capa_df, on="Canton", how="left")
                 merged_df.drop_duplicates(['datetime', 'Canton', 'operator'], inplace=True)
             except:
-                st.dataframe(forecast_df)
                 merged_df = pd.merge(forecast_df, capa_df, on="Canton", how="left")
+                st.dataframe(merged_df)
                 merged_df.drop_duplicates(['datetime', 'Canton', 'operator'], inplace=True)
 
             dt = merged_df['datetime'].min().tz_convert('CET')
