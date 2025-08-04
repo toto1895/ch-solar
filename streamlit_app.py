58b94cdd78b15cd20f5ab696ab44548129df0f0d
Toto95 <101876227+toto1895@users.noreply.github.com>
2025-08-04 20:26:58 +0000

    Refactor add_forecast_traces to improve datetime handling by resetting index after resampling

--- streamlit_app.py
+++ streamlit_app.py
@@ -872,8 +872,9 @@ def add_forecast_traces(selected_model,fig, df, name, line_width=2, color=None):
         df.set_index('datetime', inplace=True)
         df = df.resample('15min').bfill(limit=4)
         df.index.name = 'datetime'
-        #df.reset_index(inplace=True)
+        
         df['datetime'] = df.index
+        df.reset_index(drop=True)
                                        
     try:
         fig.add_trace(go.Scatter(
